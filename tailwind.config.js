cb17604187f135347a131713b11c39177b45b67a
BrightsLifE <ovi425764@gmail.com>
2022-07-29 23:41:37 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        penguin: '#fffbeb',
+        navLogoColor: '#A4BC46',
+      }
+    },
+  },
+  plugins: [],
+}
